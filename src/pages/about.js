abcecd7bd3e7df25439ff9a143ac984601e9e769
hareesh43 <hareeshgummani@gmail.com>
2021-02-25 21:07:13 +0530

    added 404 page and about page

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel optio esse
+        voluptatibus suscipit, eveniet error natus vero beatae, ullam atque
+        veritatis minus cupiditate, sint sapiente et laborum iste ab veniam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel optio esse
+        voluptatibus suscipit, eveniet error natus vero beatae, ullam atque
+        veritatis minus cupiditate, sint sapiente et laborum iste ab veniam?
+      </p>
+    </div>
+  )
+}
